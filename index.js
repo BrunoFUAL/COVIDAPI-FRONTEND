50c385a72b976ec518c6df1b5b14b02d84dc317c
Bruno Fernandes <30004461@students.ual.pt>
2021-10-31 11:46:34 +0000

    add features

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ newspapers.forEach(newspaper => {
             const html = response.data
             const $ = cheerio.load(html)
 
-            $('a:contains("climate")', html).each(function () {
+            $('a:contains("COVID-19")', html).each(function () {
                 const title = $(this).text()
                 const url = $(this).attr('href')
 
