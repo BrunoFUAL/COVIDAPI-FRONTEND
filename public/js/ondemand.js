80cfb330f4eea97738ac6c5ae0de1c8ecabca45d
Bruno Fernandes <30004461@students.ual.pt>
2021-12-04 14:56:31 +0000

    flipped cards

--- public/js/ondemand.js
+++ public/js/ondemand.js
@@ -1,45 +1,44 @@
 async function getArticles() {
-    var selected = '';
-    var ele = document.getElementsByName('btnradio');
+  var selected = "";
+  var ele = document.getElementsByName("btnradio");
 
-    for(i = 0; i < ele.length; i++) {
-        if(ele[i].checked){
-          selected = ele[i].getAttribute("value")
-        }
-
+  for (i = 0; i < ele.length; i++) {
+    if (ele[i].checked) {
+      selected = ele[i].getAttribute("value");
     }
-    const urlBase = "http://localhost:8080/news/";
-    const listArticles = document.getElementById("articles");
-    let texto = "";
-    var myHeaders = new Headers();
-    let url = urlBase + selected;
+  }
+  const urlBase = "http://localhost:8080/news/";
+  const listArticles = document.getElementById("articles");
+  let texto = "";
+  var myHeaders = new Headers();
+  let url = urlBase + selected;
 
-    var myInit = { method: "GET", headers: myHeaders };
+  var myInit = { method: "GET", headers: myHeaders };
 
-    var myRequest = new Request(url, myInit);
+  var myRequest = new Request(url, myInit);
 
-    await fetch(myRequest).then(async function (response) {
-        if (!response.ok) {
-            listArticles.innerHTML = "Can't show articles right now!";
-        } else{
-            articles = await response.json();
-            listArticles.innerHTML = ""
-            for (const article of articles) {
-                texto += `
-                <div class="card border-light" >
-                <div class="col">
-                    <div class="card-header">${article.source}</div>
-                        <div class="card">
-                            <div class="card-body">
-                            <h5 class="card-title">${article.title}</h5>
-                            <p class="card-text"><button type="button" class="btn btn-outline-primary"><a href="${article.url}">Go to site</a></button></p>
+  await fetch(myRequest).then(async function (response) {
+    if (!response.ok) {
+      listArticles.innerHTML = "Can't show articles right now!";
+    } else {
+      articles = await response.json();
+      listArticles.innerHTML = "";
+      for (const article of articles) {
+        texto += `
+                <div class="flip-card" >
+                        <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                        <img src="/images/iconcovid.png" alt="covid" style="width:100%;">
+                        <h3>${article.source}</h3>
+                        <button type="button" class="button-flipped"><a href="${article.url}">Website</a></button></p></div>
+                            <div class="flip-card-back">
+                            <h8>${article.title}</h8>
+                            
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>`; 
-            }
-            listArticles.innerHTML = texto;
-        }
-    });
+                    </div>`;
+      }
+      listArticles.innerHTML = texto;
+    }
+  });
 }
